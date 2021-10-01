e3d8a719d1f664d1af429bae7ec666f9fdf96a3e
Sten Petrov <sten.petrov@gmail.com>
2021-09-30 17:39:00 -0700

    removed unneeded exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-export { PlaywrightAvailabilityTester, PlaywrightAvailabilityTesterOptions } from "./PlaywrightAvailabilityTester";
 export { test, expect } from "./AppInsightsAvailabilityFixture";
 export { PlaywrightTestLauncher } from "./PlaywrightTestLauncher";
