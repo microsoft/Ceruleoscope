6d4aec42172fa14edacb0b7441f34893fef9ac8d
Sten Petrov <sten.petrov@gmail.com>
2021-10-12 12:12:39 -0700

    cosmetics

--- dist/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { PlaywrightAvailabilityTester, PlaywrightAvailabilityTesterOptions } from "./PlaywrightAvailabilityTester";
-export { test, expect } from "./AppInsightsAvailabilityFixture";
-export { PlaywrightTestLauncher } from "./PlaywrightTestLauncher";
